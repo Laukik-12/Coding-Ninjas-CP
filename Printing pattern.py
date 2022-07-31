b222b48c1f8ed722e9d456df2b797b622e097deb
Laukik-12 <110079607+Laukik-12@users.noreply.github.com>
2022-07-31 22:34:41 +0530

    Create Printing pattern.py
    Print the following pattern for the given number of rows.
    Pattern for N = 5
     1    2   3    4   5
     11   12  13   14  15
     21   22  23   24  25
     16   17  18   19  20
     6    7    8   9   10

---
+++ Printing pattern.py
@@ -0,0 +1,10 @@
+n = int(input())
+for i in range(int((n+1)/2)):
+    for j in range(1,n+1):
+        print(n*2*i + j ,end = " ");
+    print()
+for i in range(n//2,0,-1):
+    for j in range(1,n+1):
+        print(n*(2*i-1) + j , end = " ")
+    print()
+    
